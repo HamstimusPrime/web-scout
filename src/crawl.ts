143e50a4fa305acb13200021a89cf2958acb4914
Mohammed Dadi <artofmohammed@gmail.com>
2026-03-09 00:05:31 +0100

    feat: add HTML fetching functionality and CLI argument validation
    - Implement getHTML function to fetch web pages with proper error handling and user agent
    - Add CLI argument validation in main function
    - Begin crawlPage function structure for web crawling
    - Minor refactor to normalizeURL for trailing slash handling

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,10 +1,51 @@
+import { resolve } from "node:dns";
+
 export function normalizeURL(url: string): string {
     try {
         const u = new URL(url)
-        return u.host + u.pathname.replace(/\/$/, "")
+        let fullPath = u.host + u.pathname
+        if (fullPath.slice(-1) === "/") {
+            fullPath = fullPath.slice(0, -1);
+        }
+        return fullPath
     } catch (error) {
         console.log(`unable to parse URL, error: ${error}\n`)
         return ""
     }
-
 }
+
+export async function getHTML(url: string) {
+    try {
+        const responseObject = await fetch(url, {
+            headers: {
+                "User-Agent": "BootCrawler/1.0"
+            }
+        })
+        const responseContentType = responseObject.headers.get("content-type")
+        if (responseObject.status === 400) {
+            console.log(`could not fetch response from URL:${url}\nexiting...`)
+            process.exit(1)
+        }
+        if (responseContentType && !responseContentType.includes("text/html")) {
+            console.log("invalid response type\nexiting...")
+            process.exit(1)
+        }
+        //print html in response to console as string
+        console.log(await responseObject.text())
+
+    } catch (error) {
+        console.log(`an error occured connection to URL:${url}\nerror:${error}\nexiting...`)
+    }
+} 
+
+export function crawlPage(
+    baseURL: string,
+    currentURL: string,
+    pages: Record<string, number> = {},
+){
+    //check to see if baseURL and current URL are from the same domain
+    //first noramlize URLs of both
+    // check if both have the same domain name. if they dont
+    
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { argv } from 'node:process';
+import { getHTML } from "./crawl"
 
 
-function main(){
+function main() {
+    if (argv.length <= 2 || argv.length > 3) {
+        console.log("too many arguments passed or no arguments\nexiting...")
+        process.exit(1)
+    }
+    const urlFromUser = argv[2]
+    console.log(`CLI command: ${urlFromUser}`)
 
+    getHTML(urlFromUser)
 }
 
 main();
